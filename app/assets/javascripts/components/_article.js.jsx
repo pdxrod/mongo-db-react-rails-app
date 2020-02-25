dfcd1b8740b3ac1d06860eb6bea1721968928bf3
z <x@y.com>
2020-02-25 13:38:45 -0800

    Got save working by setting each attribute text input to a unique id, and retrieving its value

--- app/assets/javascripts/components/_article.js.jsx
+++ app/assets/javascripts/components/_article.js.jsx
@@ -11,15 +11,25 @@ class Article extends React.Component{
 
   handleEdit(){
    if(this.state.editable) {
-      let id = this.props.article.id['$oid']
       let attributes = this.props.article.attributes
-      let article = attributes
-      article["id"] = this.props.article.id
+      let article = this.props.article.attributes
       article.id = this.props.article.id
+      id = article.id['$oid']
 
 console.log("handleEdit article ", article)
 console.log("handleEdit article id ", article.id)
 console.log("handleEdit giving article to handleUpdate ", article)
+
+      Object.keys(attributes).map((key) => {
+        let input_id = "article_" + id + "_" + key
+
+        let attr = document.getElementById( input_id )
+        if( attr ) {
+        console.log("handleEdit getting value ", input_id, attr.value)
+          article[ key ] = attr.value
+
+        }
+      })
 
       this.props.handleUpdate(article)
     } else {
@@ -37,14 +47,20 @@ console.log("handleEdit giving article to handleUpdate ", article)
     let category = <h3>{this.props.article.category}</h3>
     let attributes = this.props.article.attributes
     delete attributes[ 'id' ]
-  //  this.props.handleConsole(" render "+this.props.article.attributes.name+" id "+id, false)
+    delete attributes[ 'category' ]
+
     var attrs = Object.keys(attributes).map((key) => {
       let attr = attributes[ key ]
 
-  console.log("render attr ", attr)
+  console.log("render attr ", key, attr)
+      if( ! attr ) {
+        return(
+          <div key={key}></div>
+        )
+      }
 
-      let input_name = "attributes[" + attr + "]"
-      let input_id = "attributes_" + attr
+      let input_name = "article[" + attr + "]]"
+      let input_id = "article_" + id + "_" + key
       let attribute = this.state.editable ?
           <input type='text' name={input_name} id={input_id} defaultValue={attr}/> :
           <b>{attr}</b>
